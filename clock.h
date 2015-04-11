32925c55207bbcdd0626f435c321d1c51d52ca69
Harshal Jalan <harshal.jalan@gmail.com>
2015-04-11 18:46:06 +0530

    first commit

---
+++ clock.h
@@ -0,0 +1,24 @@
+double giveR(double x, double y);
+double giveTheta(double x, double y);
+void goTO(double x, double y);
+void moveX(double x);
+void moveY(double y);
+void currentPosition(void);
+void make(char); // input 0, 1, 2...9 or : to draw it (use switch)
+void make0(void);
+void make1(void);
+void make2(void);
+void make3(void);
+void make4(void);
+void make5(void);
+void make6(void);
+void make7(void);
+void make8(void);
+void make9(void);
+void make:(void);
+void setTime(int h, int m, int s);
+void updateTime(int s); // see if arduino has default library
+void printTime(void);
+void erase(void);
+void penUp(void);
+void penDown(void);
